6c11bb606c7f82658502d51378d207ec713be569
bandittu <109623633+banditesq@users.noreply.github.com>
2022-08-25 06:59:07 +0100

    add create receiver ATA if not found.

--- core/src/createTransfer.ts
+++ core/src/createTransfer.ts
@@ -1,4 +1,4 @@
-import { createTransferCheckedInstruction, getAccount, getAssociatedTokenAddress, getMint } from '@solana/spl-token';
+import { createTransferInstruction, getAccount, getAssociatedTokenAddress, getMint,createAssociatedTokenAccountInstruction, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import {
     Commitment,
     Connection,
@@ -70,7 +70,7 @@ export async function createTransfer(
         }
 
         for (const pubkey of reference) {
-            instruction.keys.push({ pubkey, isWritable: false, isSigner: false });
+            instruction.map(e=>e.keys.push({ pubkey, isWritable: false, isSigner: false }) );
         }
     }
 
@@ -91,7 +91,7 @@ export async function createTransfer(
     }
 
     // Add the transfer instruction to the transaction
-    transaction.add(instruction);
+    instruction.map((instruction: TransactionInstruction) => transaction.add(instruction));
 
     return transaction;
 }
@@ -101,7 +101,7 @@ async function createSystemInstruction(
     amount: BigNumber,
     sender: PublicKey,
     connection: Connection
-): Promise<TransactionInstruction> {
+): Promise<TransactionInstruction[]> {
     // Check that the sender and recipient accounts exist
     const senderInfo = await connection.getAccountInfo(sender);
     if (!senderInfo) throw new CreateTransferError('sender not found');
@@ -126,11 +126,11 @@ async function createSystemInstruction(
     if (lamports > senderInfo.lamports) throw new CreateTransferError('insufficient funds');
 
     // Create an instruction to transfer native SOL
-    return SystemProgram.transfer({
+    return [SystemProgram.transfer({
         fromPubkey: sender,
         toPubkey: recipient,
         lamports,
-    });
+    })];
 }
 
 async function createSPLTokenInstruction(
@@ -139,7 +139,7 @@ async function createSPLTokenInstruction(
     splToken: PublicKey,
     sender: PublicKey,
     connection: Connection
-): Promise<TransactionInstruction> {
+): Promise<TransactionInstruction[]> {
     // Check that the token provided is an initialized mint
     const mint = await getMint(connection, splToken);
     if (!mint.isInitialized) throw new CreateTransferError('mint not initialized');
@@ -155,17 +155,39 @@ async function createSPLTokenInstruction(
     const senderAccount = await getAccount(connection, senderATA);
     if (!senderAccount.isInitialized) throw new CreateTransferError('sender not initialized');
     if (senderAccount.isFrozen) throw new CreateTransferError('sender frozen');
+    var transaction = new Transaction();
 
     // Get the recipient's ATA and check that the account exists and can receive tokens
+
     const recipientATA = await getAssociatedTokenAddress(splToken, recipient);
-    const recipientAccount = await getAccount(connection, recipientATA);
-    if (!recipientAccount.isInitialized) throw new CreateTransferError('recipient not initialized');
-    if (recipientAccount.isFrozen) throw new CreateTransferError('recipient frozen');
+
+    try {
+        const recipientAccount = await getAccount(connection, recipientATA);
+        if (recipientAccount.isFrozen) throw new CreateTransferError('recipient frozen');
+
+
+
+    } catch (error) {
+
+         transaction.add(createAssociatedTokenAccountInstruction( sender,recipientATA,recipient,mint.address ))
+
+    }
+
+
+
+    transaction.add(
+        createTransferInstruction(senderATA,recipientATA,sender,BigInt(String(amount)),undefined,TOKEN_PROGRAM_ID)
+
+    )
+
+    transaction.feePayer = sender;
+    transaction.recentBlockhash = (await connection.getRecentBlockhash("confirmed")).blockhash;
 
     // Check that the sender has enough tokens
+
     const tokens = BigInt(String(amount));
     if (tokens > senderAccount.amount) throw new CreateTransferError('insufficient funds');
 
     // Create an instruction to transfer SPL tokens, asserting the mint and decimals match
-    return createTransferCheckedInstruction(senderATA, splToken, recipientATA, sender, tokens, mint.decimals);
+    return transaction.instructions;
 }
